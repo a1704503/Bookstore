a4eb35021872874f9f35e6b17052b7fc1e79512c
Sam <ke.sk.usta@hotmail.com>
2020-10-08 01:47:25 +0300

    add dependency pom.xml, authentication urls, inMemory default users

--- src/main/java/palvelinohjelmointi/Bookstore/WebSecurityConfig.java
+++ src/main/java/palvelinohjelmointi/Bookstore/WebSecurityConfig.java
@@ -1,9 +1,17 @@
 package palvelinohjelmointi.Bookstore;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -11,8 +19,47 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin()
-				.loginPage("/login").permitAll().and().logout().permitAll();
+		http
+		.authorizeRequests()
+		.antMatchers("/", "/home")
+		.permitAll()
+		.anyRequest()
+		.authenticated()
+		.and()
+		.formLogin()
+		.loginPage("/login")
+		.defaultSuccessUrl("/booklist")
+		.permitAll()
+		.and()
+		.logout()
+		.permitAll();
 	}
+	
+	@Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+		
+        List<UserDetails> users = new ArrayList();
+        
+        //InMemory user #1
+    	UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("user")
+                .roles("USER")
+                .build();
+
+    	users.add(user);
+    	
+    	//InMemory user #2
+    	user = User.withDefaultPasswordEncoder()
+                   .username("admin")
+                   .password("admin")
+                   .roles("USER", "ADMIN")
+                   .build();
+    	
+    	users.add(user);
+    	
+        return new InMemoryUserDetailsManager(users);
+    }
 
 }
